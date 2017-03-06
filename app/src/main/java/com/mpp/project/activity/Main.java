53ef4a6155b2f41ab996ffccb09767dfb155a7df
wudizhuo <sunzhuo1228@126.com>
2017-03-06 15:24:25 -0600

    add dash board page

--- app/src/main/java/com/mpp/project/activity/Main.java
+++ app/src/main/java/com/mpp/project/activity/Main.java
@@ -5,18 +5,12 @@ import android.view.View;
 
 import com.mpp.project.R;
 
-import roboguice.inject.ContentView;
-import roboguice.inject.InjectView;
-
-@ContentView(R.layout.activity_main)
 public class Main extends BaseActivity {
-    @InjectView(R.id.btn_login)
-    public View loginBtn;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loginBtn.setOnClickListener(new View.OnClickListener() {
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(Dashboard.getIntentToMe(Main.this));
