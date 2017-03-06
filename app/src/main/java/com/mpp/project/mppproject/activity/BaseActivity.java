041359ceb2a1c5ca9ab38c7510a0caefadcdc7fa
wudizhuo <sunzhuo1228@126.com>
2017-03-06 10:10:36 -0600

    add empty activity

---
+++ app/src/main/java/com/mpp/project/mppproject/activity/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.mpp.project.mppproject.activity;
+
+import android.app.Activity;
+
+public class BaseActivity extends Activity{
+}
---
+++ app/src/main/java/com/mpp/project/mppproject/activity/Main.java
@@ -0,0 +1,4 @@
+package com.mpp.project.mppproject.activity;
+
+public class Main extends BaseActivity{
+}
