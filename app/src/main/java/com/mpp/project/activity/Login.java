499d3ea4f63ecfeb97d67cc3ad3509983cb89ac2
wudizhuo <sunzhuo1228@126.com>
2017-03-07 10:47:26 -0600

    change main to login

--- app/src/main/java/com/mpp/project/activity/Login.java
+++ app/src/main/java/com/mpp/project/activity/Login.java
@@ -5,15 +5,15 @@ import android.view.View;
 
 import com.mpp.project.R;
 
-public class Main extends BaseActivity {
+public class Login extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         findViewById(R.id.btn_login).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(Dashboard.getIntentToMe(Main.this));
+                startActivity(Dashboard.getIntentToMe(Login.this));
             }
         });
     }
