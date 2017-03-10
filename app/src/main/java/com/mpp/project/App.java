91f2f221f4cd091e71db06af99b75cecce1bc56b
wudizhuo <sunzhuo1228@126.com>
2017-03-09 18:00:05 -0600

    add fake data

--- app/src/main/java/com/mpp/project/App.java
+++ app/src/main/java/com/mpp/project/App.java
@@ -10,6 +10,7 @@ import com.mpp.project.business.Book;
 import com.mpp.project.business.Librarian;
 import com.mpp.project.business.LibraryMember;
 import com.mpp.project.business.SuperAdmin;
+import com.mpp.project.controller.BookController;
 import com.mpp.project.dataaccess.DataAccessFacade;
 
 import net.danlew.android.joda.JodaTimeAndroid;
@@ -43,7 +44,7 @@ public class App extends Application {
         Admin admin = new Admin(002, "firstName", "lastName", "password", "phoneNo");
         dataAccessFacade.savePerson(admin);
 
-        Librarian librarian = new Librarian(003,"firstName", "lastName", "password", "phoneNo");
+        Librarian librarian = new Librarian(003, "firstName", "lastName", "password", "phoneNo");
         dataAccessFacade.savePerson(librarian);
 
 
@@ -51,16 +52,39 @@ public class App extends Application {
         // So i extended the Admin, not Person in the SuperAdmin class so had to make Admin not final.
         LibraryMember member = new LibraryMember(100, "LibraryMember's firstName", "LibraryMember-lastName", "password", "123", new Address("street", "city", "state", "zip", "country"));
         LibraryMember member2 = new LibraryMember(101, "LibraryMember's firstName", "LibraryMember-lastName", "password", "123", new Address("street", "city", "state", "zip", "country"));
+        LibraryMember member3 = new LibraryMember(102, "LibraryMember's firstName", "LibraryMember-lastName", "password", "123", new Address("street", "city", "state", "zip", "country"));
+        LibraryMember member4 = new LibraryMember(104, "LibraryMember's firstName", "LibraryMember-lastName", "password", "123", new Address("street", "city", "state", "zip", "country"));
         dataAccessFacade.savePerson(member);
         dataAccessFacade.savePerson(member2);
+        dataAccessFacade.savePerson(member3);
+        dataAccessFacade.savePerson(member4);
 
         ArrayList<Author> authors = new ArrayList<>();
         authors.add(new Author("firstName", "lastName", "phoneNo", new Address("street", "city", "state", "zip", "country"), "credentials", "bio"));
         Book book = new Book("1001", "MPP-C++", "7", "5", authors);
+        book.addCopy();
+        book.addCopy();
+        book.addCopy();
         Book book2 = new Book("1002", "MPP-C++", "7", "5", authors);
+        book2.addCopy();
+        book2.addCopy();
+        book2.addCopy();
+        Book book3 = new Book("1003", "MPP-C++", "7", "5", authors);
+        book3.addCopy();
+        Book book4 = new Book("1004", "MPP-C++", "7", "5", authors);
+        book4.addCopy();
 
         dataAccessFacade.saveBook(book);
         dataAccessFacade.saveBook(book2);
+        dataAccessFacade.saveBook(book3);
+        dataAccessFacade.saveBook(book4);
+
+        BookController bookController = new BookController();
+        bookController.checkout(book, member);
+        bookController.checkout(book2, member2);
+        bookController.checkout(book3, member3);
+        bookController.checkout(book4, member4);
+
 
         AppPreferences.setHasSetupFile();
     }
